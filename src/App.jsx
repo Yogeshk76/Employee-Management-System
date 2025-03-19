ef03aa6c7e9b5f9e0041216991c5327952f2d0ae
Yogeshk76 <yogeshkumar60508@gmail.com>
2025-03-19 20:06:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,49 @@ import "./App.css";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-// import { useEffect } from 'react'
-// import { setLocalStorage } from './utils/localStorage'
-// import { getLocalStorage } from './utils/localStorage'
+import { AuthContext } from "./context/AuthProvider";
+import { useContext } from "react";
+import { useEffect } from "react";
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+
+  // useEffect(
+  //   (authData) => {
+  //     const loggedInUser = localStorage.getItem("loggedInUser");
+  //     if (loggedInUser) {
+  //       setUser(loggedInUser.role);
+  //     }
+  //   },
+  //   [authData]
+  // );
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
-      setUser("admin");
-    } else if (email == "user@me.com" && password == "123") {
-      setUser("employee");
+      setUser({ role: "admin" });
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (authData) {
+      const employee = authData.employees.find(
+        (e) => e.email === email && e.password === password
+      );
+
+      if (employee) {
+        setUser({ role: "employee" });
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "employee" })
+        );
+      } else {
+        alert("Invalid Credentials");
+      }
     } else {
       alert("Invalid Credentials");
     }
   };
 
-
-  // useEffect(() => {
-  //   setLocalStorage()
-  //   getLocalStorage()
-  // },);
+  // const data = useContext(AuthContext);
+  // console.log(data);
 
   return (
     <>
